eb960a0eea65f7ebc110e4893bd790cfdadb94a1
Krzysztof Piaskowy <krzychu2956@gmail.com>
2018-11-27 14:20:53 +0100

    add dynamic menu generator

--- Render.php
+++ Render.php
@@ -6,6 +6,7 @@ class Render{
         $html = file_get_contents('src/template/'.$template.'.html');
         $html = str_replace('{{title}}', $title, $html);
         $html = str_replace('{{content}}', $content, $html);
+        $html = str_replace('{{menu}}', Menu::getMenu(), $html);
         echo $html;
     }
 
---
+++ components/Menu.php
@@ -0,0 +1,27 @@
+<?php
+
+class Menu{
+
+  public static function getMenu(){
+      $db = ORM::forTable('pages')
+          ->select('url')
+          ->select('title')
+          ->where('active', 1)
+          ->orderByAsc('menu_order')
+          ->findArray();
+
+    $html = '<div class="menu-wrap"><nav class="menu navbar"><div class="icon-list navbar-collapse"><ul class="navbar-nav">{{lista}}</ul></div></nav><button class="close-button" id="close-button"><i class="lnr lnr-cross"></i></button></div>';
+
+    $lista = '';
+    foreach($db as $item){
+      $lista .= self::makeItem($item['url'], $item['title']);
+    }
+
+    return str_replace('{{lista}}', $lista, $html);
+  }
+
+  private function makeItem($url, $title){
+    return '<li class="nav-item"><a class="nav-link" href=".'.$url.'">'.$title.'</a></li>';
+  }
+
+}
